8e99955ee0c91a0c0956493ee242d13a823d8876
InshaM <42205104+InshaM@users.noreply.github.com>
2018-10-12 10:34:37 +0530

    Update lab7_q10.cpp

--- lab7_q10.cpp
+++ lab7_q10.cpp
@@ -1,13 +1,13 @@
-//including library
+//include in the library.
 #include <iostream>
 using namespace std;
 int fib(int n){
-	//conditions
+	//the required conditions are then given .
 	if(n==1){
 	return 0;}
 	if(n==2){
 	return 1;}
-	//m the tern of fibonacci sequence
+	//n-th term of fibonacci sequence.
 	else{
 	return fib(n-1)+fib(n-2);}
 }
@@ -15,13 +15,13 @@ int fib(int n){
 
 int main(){
 	int a;
-	//asking a number
+	//asking a number to the user.
 	cout<<"Which term in Fibonacci sequence do you want?";
 	cout<<endl;
 	cin>>a;
 	cout<<endl;
-	//call for function
-	cout<<"The "<<a<<"th term of Fibenocci sequence is "<<fib(a)<<endl;
+	//call for function :
+	cout<<"The "<<a<<"th term of Fibonacci sequence is : "<<fib(a)<<endl;
 return 0;
 
 }
