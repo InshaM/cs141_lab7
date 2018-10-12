ad739f623beb29b8d992eca3b458bc2b3a1be48f
InshaM <42205104+InshaM@users.noreply.github.com>
2018-10-12 11:13:11 +0530

    Update lab7_q8.cpp

--- lab7_q8.cpp
+++ lab7_q8.cpp
@@ -1,21 +1,21 @@
-//including library
+//including in the library
 #include <iostream>
 using namespace std;
 int sum(int n){
-	//conditions
+	//conditions required :
 	if(n==0){
 	return 0;}
-	//adding the sum of the digits of the number
+	//adding the sum of the digits of the number.
 	else{
 	return n%10 + sum(n/10);}
 
 }
 int main(){
 	int n;
-	//asking a number
-	cout<<"type a number : ";cin>>n;
+	//asking a number from the user
+	cout<<"Type a number you like : ";cin>>n;
 	cout<<endl;
-	//call for function
+	//call for the required function :
 	cout<<"The sum of the digits of the number "<<n<<" is "<<sum(n)<<endl;
 return 0;
 
