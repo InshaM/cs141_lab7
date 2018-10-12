e162b20fb236da7645af6bc629564c0b3291508b
InshaM <42205104+InshaM@users.noreply.github.com>
2018-10-12 10:30:41 +0530

    Update lab7_q1.cpp

--- lab7_q1.cpp
+++ lab7_q1.cpp
@@ -1,23 +1,23 @@
-//include library
+//including in the library
 #include <iostream>
 using namespace std;
-//function inside a function
+//function inside another function
 int number_Power(int base, int power){
-	//conditions for the function to execute
+	//conditions for the function to be executed
 	if(power==0){	
 	return 1;}
-	//this is similar to looping
+	//this is more or less similar to looping conditions
 	else {
 	return base*number_Power(base,power-1);}}
 
 int main (){
 	int base; int power;
-	//asking user for number
+	//asking user for a number they want.
 	cout <<"Enter the base : ";
 	cin>>base;cout<<endl;
 	cout <<"Enter the power : ";
 	cin>>power;cout<<endl;
-	//the result
-	cout<<base<<"^"<<power<<" is "<</*callimg function*/number_Power(base,power)<<endl;
+	//the result is
+	cout<<base<<"^"<<power<<" is "<</*calling function*/number_Power(base,power)<<endl;
 return 0;
 }
